e70809b8ed409c42b4768f422f5aba454404f597
Ahmed Mhiri <ahmedmhiri218@gmail.com>
2023-11-06 04:13:08 +0100

    launch

--- scripts/launch.fish
+++ scripts/launch.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/fish
 
 if test -z "$argv[1]"
-  echo "Please provide an argument"
-  exit 1
+    echo "Please provide an argument"
+    exit 1
 end
 
 set arg $argv[1]
@@ -10,7 +10,7 @@ set arg $argv[1]
 
 function isRunning
     set appName $argv[1]
-    if pgrep -x $appName > /dev/null
+    if pgrep -x $appName >/dev/null
         return 0
     else
         return 1
@@ -23,57 +23,59 @@ function getWorkspaceId
     if test -n "$client"
         echo $client
     else
-        echo "9"  # Return 9 to indicate that the application is not running in any workspace
+        echo 9 # Return 9 to indicate that the application is not running in any workspace
     end
 end
 
 switch $arg
-  case "browser"
-    if isRunning "firefox"
-      hyprctl dispatch workspace (getWorkspaceId "firefox")
-    else
-      firefox
-    end
+    case browser
+        if isRunning firefox
+            hyprctl dispatch workspace (getWorkspaceId "firefox")
+        else
+            firefox
+        end
 
-  case "code"
-    code --password-store="gnome" \
-      --ozone-platform=wayland \
-      --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer
+    case code
+        code --password-store="gnome" \
+            --ozone-platform=wayland \
+            --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer
 
-  case "rofi"
-    rofi -show drun
+    case rofi
+        rofi -show drun
 
-  case "terminal"
-    kitty
+    case terminal
+        kitty
 
-  case "tmux"
-    kitty --detach -T tmux -e ~/.config/hypr/scripts/tmux_session.fish
+    case zellij
+        zellij -s Main -l example
+    case tmux
+        kitty --detach -T tmux -e ~/.config/hypr/scripts/tmux_session.fish
 
-  case "neovim"
-      kitty --detach --hold -e nvim
-  case "neovide"
-      neovide --multigrid
+    case neovim
+        kitty --detach --hold -e nvim
+    case neovide
+        neovide --multigrid
 
-  case "lunarvim"
-      kitty --detach --hold -e lvim
-  case "lunarvide"
-      neovide --neovim-bin ~/.local/bin/lvim --multigrid
+    case lunarvim
+        kitty --detach --hold -e lvim
+    case lunarvide
+        neovide --neovim-bin ~/.local/bin/lvim --multigrid
 
-  case "graphicalFiles"
-    nemo
+    case graphicalFiles
+        nemo
 
-  case "files"
-    kitty --detach -e "ranger"
+    case files
+        kitty --detach -e ranger
 
-  case "clipboard"
-    cliphist list | rofi -dmenu | cliphist decode | wl-copy
+    case clipboard
+        cliphist list | rofi -dmenu | cliphist decode | wl-copy
 
-  case "spotify"
-    spotify
+    case spotify
+        spotify
 
-  case "spt"
-    spotifyd
-    kitty --detach -e "spt"
-    kitty --detach -e "cava"
+    case spt
+        spotifyd
+        kitty --detach -e spt
+        kitty --detach -e cava
 
 end
