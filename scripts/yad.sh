25badce98a1f80b4177e73a955e527c795249286
Mhirii <ahmedmhiri218@gmail.com>
2023-11-30 09:09:44 +0100

    yad

--- scripts/yad.sh
+++ scripts/yad.sh
@@ -11,19 +11,21 @@ yad --width=330 --height=360 \
 	--column= \
 	--timeout=6000 \
 	--no-escape \
+	--hscroll-policy=never \
+	--vscroll-policy=never \
 	--no-click \
 	--fontname='FiraCode Nerd Font' \
 	--no-selection \
 	--timeout-indicator=right \
 	"ESC" "󰗼 " "Exit Submap" \
-	"+T" " " "lxtask" \
-	"+A" " " "Alacritty" \
-	"+S" "󰓇 " "Spotify" \
-	"+󰘶 +S" "󰓇 " "spt" \
-	"+D" "󰙯 " "discord" \
-	"+G" "󰊤 " "open repo from clipboard" \
-	"+K" " " "Kitty" \
-	"+C" "󰨞 " "Code" \
-	"+B" " " "Bitwarden" \
-	"+N" " " "Neovim" \
-	"+󰘶 +N" "󱕅 " "Neovide"
+	"T" " " "lxtask" \
+	"A" " " "Alacritty" \
+	"S" "󰓇 " "Spotify" \
+	"󰘶 S" "󰓇 " "spt" \
+	"D" "󰙯 " "discord" \
+	"G" "󰊤 " "open repo from clipboard" \
+	"K" " " "Kitty" \
+	"C" "󰨞 " "Code" \
+	"B" " " "Bitwarden" \
+	"N" " " "Neovim" \
+	"󰘶 N" "󱕅 " "Neovide"
